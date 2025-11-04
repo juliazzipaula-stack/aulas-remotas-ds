301f0885a51dff47b7818fd640c9a71482af9cda
juliazzi_026 <juliazzi.paula@gmail.com>
2025-11-03 23:19:48 -0500

    feat: add text readme

---
+++ atividade1.py
@@ -0,0 +1,7 @@
+nome = input ("Qual é o seu nome? ")
+idade = int (input("Qual é a sua idade? "))
+
+if idade >= 18:
+    print(f"Olá, {nome}, você é maior de idade.")
+    else:
+        print(f"Olá, {nome}, você é menor de idade.")
