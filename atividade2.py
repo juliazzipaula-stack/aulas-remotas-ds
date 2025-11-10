7561d7def9a4d800bbad5b1dda46e953a9db2348
juliazzi_026 <juliazzi.paula@gmail.com>
2025-11-09 19:54:05 -0500

    feat: add text readme

--- atividade2.py
+++ atividade2.py
@@ -1,5 +1,12 @@
-número = int(input("Digite um número para ver a tabuada: "))
+# Pedido ao usuário um número e conversão para inteiro
+numero_str = input("Digite um número para ver a sua tabuada: ")
+numero = int(numero_str)
 
-print(f"---Tabuada do {número}---")
-for i in range(1,11):
-    print(f"{número} x {i} = {número * i}")
+print(f"-- Tabuada do {numero} --")
+
+# Laço de repetição para calcular e mostrar a tabuada
+for i in range(1, 11):
+    # Cálculo do resultado da multiplicação
+    resultado = numero * i
+    # Exibição do resultado formatado
+    print(f"{numero} x {i} = {resultado}")
