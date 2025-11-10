5c5b017e12e9f7be29c7b0dc542d890b7815fa22
juliazzi_026 <juliazzi.paula@gmail.com>
2025-11-09 19:53:28 -0500

    feat: add text readme

--- atividade1.py
+++ atividade1.py
@@ -1,7 +1,16 @@
-nome = input ("Qual é o seu nome? ")
-idade = int (input("Qual é a sua idade? "))
+# Pergunta o nome e guarda na variável 'nome'
+nome = input("Qual é o seu nome? ")
 
+# Pergunta a idade e guarda na variável 'idade_texto'
+idade_texto = input("Qual a sua idade? ")
+
+# Converte a idade de texto para inteiro e guarda na variável 'idade'
+idade = int(idade_texto)
+
+# Cria a lógica condicional para verificar se a idade é maior ou igual a 18
 if idade >= 18:
-    print(f"Olá, {nome}, você é maior de idade.")
-    else:
-        print(f"Olá, {nome}, você é menor de idade.")
+    # Mensagem para usuário maior de idade
+    print(f"Olá {nome}, você é maior de idade.")
+else:
+    # Mensagem para usuário menor de idade
+    print(f"Olá {nome}, você é menor de idade.")
